eae5863ff2d98e342ae5f1c3049de743e95b4f98
fukata <tatsuya.fukata@gmail.com>
2023-03-04 09:52:54 +0900

    add healthcheck

--- index.js
+++ index.js
@@ -5,10 +5,18 @@ import Bolt from '@slack/bolt';
 const { App } = Bolt;
 import { registerOpenai } from './openai.js';
 
-
 const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   token: process.env.SLACK_BOT_TOKEN,
+  customRoutes: [
+    {
+      path: '/up',
+      method: 'GET',
+      handler: (req, res) => {
+        res.writeHead(200, {'Content-Type': 'text/plain'}).end('OK');
+      }
+    }
+  ],
 });
 
 registerOpenai(app);
